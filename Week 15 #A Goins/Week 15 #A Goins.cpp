4a01a5b29067962accfa762c155bad13a074b401
goins65504 <goins65504@mvctc.com>
2024-04-18 09:24:49 -0400

    Renamed character.h to exceptions.h Made function testCase Added invalidCaseTransition

--- Week 15 #A Goins/Week 15 #A Goins.cpp
+++ Week 15 #A Goins/Week 15 #A Goins.cpp
@@ -4,38 +4,62 @@
 // Exceptions, Templates, and Standard Template Library (STL) #A
 
 #include"goinsLib.h"
-#include"character.h"
+#include"exceptions.h"
 
-char characterj(char start, int offset);
+char character(char start, int offset);																			// Functions
+void testCase(char, char, int);
 
 int main() {
-	char h = 'h';
-	int g = 3;
+	char multiuseChar = 'a';																					// Variables
+	int multiuseNum = 1;
+	char multiuseCase = ' ';
 
-	try {
-		char t = character(h,g);
-		cout << "The offset char is " << t << endl;
-	}
-	catch (character::invalidCharacterException) {
-
-	}
-	catch (character::invalidRangeExecption) {
-
-	}
+	testCase(multiuseCase, multiuseChar, multiuseNum);															// Test cases with different values
+	multiuseCase = ' ';
+	multiuseNum = -1;
+	testCase(multiuseCase, multiuseChar, multiuseNum);
+	multiuseCase = ' ';
+	multiuseChar = 'Z';
+	testCase(multiuseCase, multiuseChar, multiuseNum);
+	multiuseCase = ' ';
+	multiuseChar = '?';
+	multiuseNum = 5;
+	testCase(multiuseCase, multiuseChar, multiuseNum);
 
 	endProgram();
 }
 
-char characterj(char start, int offset)
+char character(char start, int offset)																			// Checks to make sure everything is valid if not throws and exception
 {
 	if (!isalpha(start))
 	{
-		throw character::invalidCharacterException();
+		throw exceptions::invalidCharacterException();
 	}
 	if (!isalpha(start + offset))
 	{
-		throw character::invalidRangeException();
+		throw exceptions::invalidRangeException();
 	}
 	char newChar = start + offset;
+	if (tolower(start) == tolower(newChar))
+	{
+		throw exceptions::invalidCaseTransition();
+	}
 	return newChar;
+}
+
+void testCase(char multiuseCase, char multiuseChar, int multiuseNum)										// Displays the offset character, or tells the user what exception they triggered
+{
+	try {
+		multiuseCase = character(multiuseChar, multiuseNum);
+		cout << "The offset char is " << multiuseCase << "\n\n";
+	}
+	catch (exceptions::invalidCharacterException) {
+		cout << "Invalid character execption\n\n";
+	}
+	catch (exceptions::invalidRangeException) {
+		cout << "Invalid range execption\n\n";
+	}
+	catch (exceptions::invalidCaseTransition) {
+		cout << "Invalid case transition";
+	}
 }
---
+++ Week 15 #A Goins/exceptions.h
@@ -0,0 +1,15 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+class exceptions {
+private:
+public:
+	class invalidCharacterException
+	{	};
+	class invalidRangeException
+	{	};
+	class invalidCaseTransition
+	{	};
+};
+
+#endif 
